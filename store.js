f9a9f6b661d832a4afc8d4ed7cff2136256c8f2c
crazyLegoid <60506835+crazyLegoid@users.noreply.github.com>
2025-12-23 12:05:43 +0530

    Update store.js

--- store.js
+++ store.js
@@ -3,28 +3,12 @@ const Store = {
         exercises: JSON.parse(localStorage.getItem('exercises')) || [],
         workouts: JSON.parse(localStorage.getItem('workouts')) || []
     },
-
     save() {
         localStorage.setItem('exercises', JSON.stringify(this.state.exercises));
         localStorage.setItem('workouts', JSON.stringify(this.state.workouts));
     },
-
-    addExercise(name, muscle) {
-        const newEx = { id: Date.now().toString(), name, muscle };
-        this.state.exercises.push(newEx);
-        this.save();
-        return newEx;
-    },
-
-    addWorkout(exerciseId, sets) {
-        const workout = {
-            id: Date.now().toString(),
-            exerciseId,
-            timestamp: new Date().toISOString(),
-            sets, // [{reps, weight}]
-            totalVolume: sets.reduce((acc, s) => acc + (s.reps * s.weight), 0)
-        };
-        this.state.workouts.push(workout);
+    addExercise(name) {
+        this.state.exercises.push({ id: Date.now().toString(), name });
         this.save();
     }
 };
